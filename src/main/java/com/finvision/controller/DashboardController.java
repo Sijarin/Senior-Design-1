a730854b210043afeecdb108df0f631a6a6bd039
Smarica <sxc3497@mavs.uta.edu>
2025-10-12 16:19:50 -0500

    Added AuthController class for login authorization

--- src/main/java/com/finvision/controller/DashboardController.java
+++ src/main/java/com/finvision/controller/DashboardController.java
@@ -5,19 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
-    
+
     @GetMapping("/")
     public String home() {
         return "Dashboard";
     }
-    
+
     @GetMapping("/dashboard")
     public String dashboard() {
         return "Dashboard";
     }
-    
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+
 }
