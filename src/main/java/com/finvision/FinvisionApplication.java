9acb4f361848106ddb4daa373884e5326c3e307b
Smarica <sxc3497@mavs.uta.edu>
2026-02-09 10:45:41 -0600

    Dashboard enhancement and addition of features

--- src/main/java/com/finvision/FinvisionApplication.java
+++ src/main/java/com/finvision/FinvisionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FinvisionApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(FinvisionApplication.class, args);
     }
-
 }
